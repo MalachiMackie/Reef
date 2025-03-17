329e7ef484200cd93e2a538be4dc5f6cc887b9b8
Malachi Mackie <malachi@mackie.co.nz>
2025-03-18 12:37:37 +1300

    multi token parsing

--- NewLang.Core.Tests/ParserTests.cs
+++ NewLang.Core.Tests/ParserTests.cs
@@ -5,38 +5,113 @@ namespace NewLang.Core.Tests;
 public class ParserTests
 {
     private readonly Parser _parser = new();
-    
+
     [Theory]
-    [MemberData(nameof(SingleTokensData))]
-    public void SingleTokens(string source, IEnumerable<Token> expectedTokens)
+    [MemberData(nameof(TestCases))]
+    public void Tests(string source, IEnumerable<Token> expectedTokens)
     {
         var result = _parser.Parse(source);
 
         result.Should().BeEquivalentTo(expectedTokens);
     }
 
-    public static IEnumerable<object[]> SingleTokensData()
+    public static IEnumerable<object[]> TestCases() =>
+    [
+        // empty source
+        ["", Array.Empty<Token>()],
+        [" ", Array.Empty<Token>()],
+        ["  ", Array.Empty<Token>()],
+        ["\t\t", Array.Empty<Token>()],
+        
+        // single tokens
+        [ "pub", new [] { Token.Pub() } ],
+        [ "fn", new [] { Token.Fn() } ],
+        [ "int", new [] { Token.IntKeyword() } ],
+        [ "(", new [] { Token.LeftParenthesis() } ],
+        [ ")", new [] { Token.RightParenthesis() } ],
+        [ "{", new [] { Token.LeftBrace() } ],
+        [ "}", new [] { Token.RightBrace() } ],
+        [ "<", new [] { Token.LeftAngleBracket() } ],
+        [ ">", new [] { Token.RightAngleBracket() } ],
+        [ ",", new [] { Token.Comma() } ],
+        [ "hello", new [] { Token.Identifier("hello") } ],
+        [ ":", new [] { Token.Colon() } ],
+        [ ";", new [] { Token.Semicolon() } ],
+        [ "result", new [] { Token.Result() } ],
+        [ "ok", new [] { Token.Ok() } ],
+        [ "error", new [] { Token.Error() } ],
+        [ "\"hello this is a string\"", new [] { Token.StringLiteral("hello this is a string") } ],
+        [ "5", new [] { Token.IntLiteral(5) } ],
+        [ "?", new [] { Token.QuestionMark() } ],
+        
+        // single token padding tests
+        [" fn ", new [] { Token.Fn() }],
+        ["fn ", new [] { Token.Fn() }],
+        [" fn", new [] { Token.Fn() }],
+        ["\tfn\t", new [] { Token.Fn() }],
+        
+        // two tokens separated by whitespace
+        ["pub fn", new [] { Token.Pub(), Token.Fn() }],
+        ["pub 5", new [] { Token.Pub(), Token.IntLiteral(5) }],
+        ["5 pub", new [] { Token.IntLiteral(5), Token.Pub() }],
+        ["\"string\" pub", new [] { Token.StringLiteral("string"), Token.Pub() }],
+        
+        // multiple tokens without separation
+        ["int)", new[] { Token.IntKeyword(), Token.RightParenthesis()}],
+        ["a)", new[] { Token.Identifier("a"), Token.RightParenthesis()}],
+        ["i)", new[] { Token.Identifier("i"), Token.RightParenthesis()}],
+        ["a)?;", new[] { Token.Identifier("a"), Token.RightParenthesis(), Token.QuestionMark(), Token.Semicolon()}],
+        
+        // two tokens padding tests
+        [" pub fn ", new [] { Token.Pub(), Token.Fn() }],
+        ["pub fn ", new [] { Token.Pub(), Token.Fn() }],
+        [" pub fn", new [] { Token.Pub(), Token.Fn() }],
+        [" pub  fn", new [] { Token.Pub(), Token.Fn() }],
+        ["\tpub\tfn\t", new [] { Token.Pub(), Token.Fn() }],
+        
+        // full source
+        [
+            """
+            pub fn DoSomething(a: int): result<int, string> {
+                var b = 2;
+                
+                if (a == b) {
+                    return ok(a);
+                }
+            }
+            """,
+            new []
+            {
+                Token.Pub(), Token.Fn(), Token.Identifier("DoSomething"), Token.LeftParenthesis(), Token.Identifier("a"),
+                Token.Colon(), Token.IntKeyword(), Token.RightParenthesis(), Token.Colon(), Token.Result(), Token.LeftAngleBracket(),
+                Token.IntKeyword(), Token.Comma(), Token.StringKeyword(), Token.RightAngleBracket(), Token.LeftBrace(),
+                Token.Var(), Token.Identifier("b"), Token.Equals(), Token.IntLiteral(2), Token.Semicolon(), Token.If(), Token.LeftParenthesis(),
+                Token.Identifier("a"), Token.DoubleEquals(), Token.Identifier("b"), Token.RightParenthesis(), Token.LeftBrace(),
+                Token.Return(), Token.Ok(), Token.LeftParenthesis(), Token.Identifier("a"), Token.RightParenthesis(), Token.Semicolon(),
+                Token.RightBrace(), Token.RightBrace()
+            }
+        ]
+    ];
+    
+    public static IEnumerable<object[]> SomeTestCases() => 
+    [
+        [")>;", Token.RightParenthesis(), 1],
+        [">;", Token.RightAngleBracket(), 1],
+        [";", Token.Semicolon(), 1],
+        [" ;", Token.Semicolon(), 2],
+        ["int):", Token.IntKeyword(), 3],
+        [" int):", Token.IntKeyword(), 4],
+    ];
+
+    [Theory]
+    [MemberData(nameof(SomeTestCases))]
+    public void SomeTests(string source, Token expectedToken, int expectedOffset)
     {
-        return [
-            [ "pub", new [] { Token.Pub() } ],
-            [ "fn", new [] { Token.Fn() } ],
-            [ "int", new [] { Token.IntKeyword() } ],
-            [ "(", new [] { Token.LeftParenthesis() } ],
-            [ ")", new [] { Token.RightParenthesis() } ],
-            [ "{", new [] { Token.LeftBrace() } ],
-            [ "}", new [] { Token.RightBrace() } ],
-            [ "<", new [] { Token.LeftAngleBracket() } ],
-            [ ">", new [] { Token.RightAngleBracket() } ],
-            [ ",", new [] { Token.Comma() } ],
-            [ "hello", new [] { Token.Identifier("hello") } ],
-            [ ":", new [] { Token.Colon() } ],
-            [ ";", new [] { Token.Semicolon() } ],
-            [ "result", new [] { Token.Result() } ],
-            [ "ok", new [] { Token.Ok() } ],
-            [ "error", new [] { Token.Error() } ],
-            [ "\"hello this is a string\"", new [] { Token.StringLiteral("hello this is a string") } ],
-            [ "5", new [] { Token.IntLiteral(5) } ],
-            [ "?", new [] { Token.QuestionMark() } ],
-        ];
+        var result = Parser.EatToken(source);
+        result.Should().NotBeNull();
+        var (token, offset) = result.Value;
+
+        token.Should().Be(expectedToken);
+        offset.Should().Be(expectedOffset);
     }
 }
--- NewLang.Core/Parser.cs
+++ NewLang.Core/Parser.cs
@@ -1,5 +1,5 @@
 ﻿using System.Buffers;
-using System.Reflection.Metadata.Ecma335;
+using System.Diagnostics.CodeAnalysis;
 
 namespace NewLang.Core;
 
@@ -47,71 +47,202 @@ public class Parser
 {
     private static readonly SearchValues<char> Digits = SearchValues.Create("0123456789");
 
-    public IEnumerable<Token> Parse(string source)
+    public IEnumerable<Token> Parse(string sourceStr)
     {
-        var tokenStart = 0;
-        var foundValue = false;
-        for (var i = 0; i < source.Length; i++)
+        var startIndex = 0;
+        var endIndex = sourceStr.Length - 1;
+        var nextToken = EatToken(sourceStr.AsSpan()[startIndex..(endIndex + 1)]);
+        while (nextToken.HasValue)
         {
-            var inStringToken = source[tokenStart] == '"';
-            // todo: this completely ignores escaping a quote mark
-            if (inStringToken)
-            {
-                if (source[i] == '"' && i != tokenStart)
-                {
-                    yield return Token.StringLiteral(source[(tokenStart + 1)..i]);
-                    foundValue = false;
-                }
-
-                continue;
-            }
+            var (token, offset) = nextToken.Value;
+            yield return token;
+            startIndex += offset;
             
-            var isTokenEnd = char.IsWhiteSpace(source[i]) || i == source.Length - 1;
-            foundValue |= isTokenEnd;
-            if (isTokenEnd)
-            {
-                var potentialToken = source.AsSpan()[tokenStart..(i + 1)];
-                // next token
-                yield return potentialToken switch
-                {
-                    "pub" => Token.Pub(),
-                    "fn" => Token.Fn(),
-                    "int" => Token.IntKeyword(),
-                    "result" => Token.Result(),
-                    ":" => Token.Colon(),
-                    "string" => Token.StringKeyword(),
-                    "<" => Token.LeftAngleBracket(),
-                    ">" => Token.RightAngleBracket(),
-                    "(" => Token.LeftParenthesis(),
-                    ")" => Token.RightParenthesis(),
-                    "{" => Token.LeftBrace(),
-                    "}" => Token.RightBrace(),
-                    ";" => Token.Semicolon(),
-                    "," => Token.Comma(),
-                    "ok" => Token.Ok(),
-                    "error" => Token.Error(),
-                    "?" => Token.QuestionMark(),
-                    "return" => Token.Return(),
-                    _ => OtherToken(potentialToken)
-                };
-                foundValue = false;
-            }
+            nextToken = EatToken(sourceStr.AsSpan()[startIndex..(endIndex + 1)]);
         }
     }
 
-    private static Token OtherToken(ReadOnlySpan<char> things)
+    public static (Token Token, int NextCharOffset)? EatToken(ReadOnlySpan<char> source)
     {
-        if (things.ContainsAnyExcept(Digits))
-        { 
-            // todo: not everything can be an identifier
-            return Token.Identifier(things.ToString());
-        }
-
-        if (int.TryParse(things, out var result))
+        if (source.IsWhiteSpace())
         {
-            return Token.IntLiteral(result);
+            return null;
         }
 
-        throw new Exception("It failed");
+        var potentialTokens = Enum.GetValues<TokenType>().ToList();
+
+        for (var i = 0; i < source.Length; i++)
+        {
+            var part = source[..(i + 1)];
+            if (part.IsWhiteSpace())
+            {
+                continue;
+            }
+
+            var trimmed = part.TrimStart();
+            
+            var nextPotentialTokens = new List<TokenType>();
+            foreach (var type in potentialTokens)
+            {
+                if (IsPotentiallyValid(type, trimmed))
+                {
+                    nextPotentialTokens.Add(type);
+                }
+            }
+
+            if (nextPotentialTokens.Count == 0)
+            {
+                // previously we had multiple potential tokens, now we have none. need to resolve to one.
+                // this will happen in the case of int)
+                return (ResolveToken(trimmed[..^1], potentialTokens), i);
+            }
+
+            potentialTokens = nextPotentialTokens;
+        }
+
+        if (potentialTokens.Count == 0)
+        {
+            return null;
+        }
+
+        return (ResolveToken(source.TrimStart(), potentialTokens), source.Length);
+    }
+
+    private static Token ResolveToken(ReadOnlySpan<char> source, IEnumerable<TokenType> tokenTypes)
+    {
+        foreach (var tokenType in tokenTypes)
+        {
+            if (tokenType == TokenType.Identifier)
+            {
+                continue;
+            }
+
+            if (TryResolveToken(source, tokenType, out var token))
+            {
+                return token.Value;
+            }
+        }
+
+        return Token.Identifier(source.ToString());
+    }
+    
+    private static bool TryResolveToken(ReadOnlySpan<char> source, TokenType type, [NotNullWhen(true)] out Token? token)
+    {
+        token = type switch
+        {
+            TokenType.Identifier => Token.Identifier(source.ToString()),
+            TokenType.If when source is "if" => Token.If(),
+            TokenType.LeftParenthesis when source is "(" => Token.LeftParenthesis(),
+            TokenType.RightParenthesis when source is ")" => Token.RightParenthesis(),
+            TokenType.Semicolon when source is ";" => Token.Semicolon(),
+            TokenType.LeftBrace when source is "{" => Token.LeftBrace(),
+            TokenType.RightBrace when source is "}" => Token.RightBrace(),
+            TokenType.Pub when source is "pub" => Token.Pub(),
+            TokenType.Fn when source is "fn" => Token.Fn(),
+            TokenType.IntKeyword when source is "int" => Token.IntKeyword(),
+            TokenType.Colon when source is ":" => Token.Colon(),
+            TokenType.LeftAngleBracket when source is "<" => Token.LeftAngleBracket(),
+            TokenType.RightAngleBracket when source is ">" => Token.RightAngleBracket(),
+            TokenType.Var when source is "var" => Token.Var(),
+            TokenType.Equals when source is "=" => Token.Equals(),
+            TokenType.Comma when source is "," => Token.Comma(),
+            TokenType.DoubleEquals when source is "==" => Token.DoubleEquals(),
+            TokenType.Else when source is "else" => Token.Else(),
+            TokenType.IntLiteral when int.TryParse(source, out var intValue) => Token.IntLiteral(intValue),
+            TokenType.StringLiteral when source[0] == '"'
+                                         && source[^1] == '"'
+                                         && source[1..].IndexOf('"') == source.Length - 2 => Token.StringLiteral(source[1..^1].ToString()),
+            TokenType.StringKeyword when source is "string" => Token.StringKeyword(),
+            TokenType.Result when source is "result" => Token.Result(),
+            TokenType.Ok when source is "ok" => Token.Ok(),
+            TokenType.Error when source is "error" => Token.Error(),
+            TokenType.QuestionMark when source is "?" => Token.QuestionMark(),
+            TokenType.Return when source is "return" => Token.Return(),
+            _ => null
+        };
+
+        return token.HasValue;
+    }
+
+    private static readonly SearchValues<char> AlphaNumeric =
+        SearchValues.Create("abcdefghijklmnopqrstuvwxyzABCDEFHIJKLMNOPQRSTUVWXYZ0123456789");
+
+    private static bool IsPotentiallyValid(TokenType type, ReadOnlySpan<char> source)
+    {
+        switch (type)
+        {
+            case TokenType.Identifier:
+                return !source.ContainsAnyExcept(AlphaNumeric);
+            case TokenType.If:
+                return "if".AsSpan().StartsWith(source) && source.Length <= "if".Length;
+            case TokenType.LeftParenthesis:
+                return source is "(";
+            case TokenType.RightParenthesis:
+                return source is ")";
+            case TokenType.Semicolon:
+                return source is ";";
+            case TokenType.LeftBrace:
+                return source is "{";
+            case TokenType.RightBrace:
+                return source is "}";
+            case TokenType.Pub:
+                return "pub".AsSpan().StartsWith(source) && source.Length <= "pub".Length;
+            case TokenType.Fn:
+                return "fn".AsSpan().StartsWith(source) && source.Length <= "fn".Length;
+            case TokenType.IntKeyword:
+                return "int".AsSpan().StartsWith(source) && source.Length <= "int".Length;
+            case TokenType.Colon:
+                return source is ":";
+            case TokenType.LeftAngleBracket:
+                return source is "<";
+            case TokenType.RightAngleBracket:
+                return source is ">";
+            case TokenType.Var:
+                return "var".AsSpan().StartsWith(source) && source.Length <= "var".Length;
+            case TokenType.Equals:
+                return source is "=";
+            case TokenType.Comma:
+                return source is ",";
+            case TokenType.DoubleEquals:
+                return "==".AsSpan().StartsWith(source) && source.Length <= "==".Length;
+            case TokenType.Else:
+                return "else".AsSpan().StartsWith(source) && source.Length <= "else".Length;
+            case TokenType.IntLiteral:
+                return !source.ContainsAnyExcept(Digits);
+            case TokenType.StringLiteral:
+            {
+                if (source[0] != '"')
+                {
+                    return false;
+                }
+
+                // single quote mark will be the beginning of a string literal
+                if (source.Length == 1)
+                {
+                    return true;
+                }
+
+                var rest = source[1..];
+
+                var nextQuoteIndex = rest.IndexOf('"');
+                
+                // the next quote either doesn't exist, or is at the end of the source
+                return nextQuoteIndex < 0 || nextQuoteIndex == rest.Length - 1;
+            }
+            case TokenType.StringKeyword:
+                return "string".AsSpan().StartsWith(source) && source.Length <= "string".Length;
+            case TokenType.Result:
+                return "result".AsSpan().StartsWith(source) && source.Length <= "result".Length;
+            case TokenType.Ok:
+                return "ok".AsSpan().StartsWith(source) && source.Length <= "ok".Length;
+            case TokenType.Error:
+                return "error".AsSpan().StartsWith(source) && source.Length <= "error".Length;
+            case TokenType.QuestionMark:
+                return source is "?";
+            case TokenType.Return:
+                return "return".AsSpan().StartsWith(source) && source.Length <= "return".Length;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(type), type, null);
+        }
     }
 }
