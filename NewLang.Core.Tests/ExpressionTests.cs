969f962e4c3aa70eee3bf2d769ed1e265f98600b
Malachi Mackie <malachi@mackie.co.nz>
2025-03-20 21:06:37 +1300

    initial expression setup

--- NewLang.Core.Tests/ExpressionTests.cs
+++ NewLang.Core.Tests/ExpressionTests.cs
@@ -1,6 +1,23 @@
-﻿namespace NewLang.Core.Tests;
+﻿using FluentAssertions;
+
+namespace NewLang.Core.Tests;
 
 public class ExpressionTests
 {
+    private readonly ExpressionBuilder _expressionBuilder = new();
     
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void Tests(IEnumerable<Token> tokens, IEnumerable<Expression> expectedExpression)
+    {
+        var expression = _expressionBuilder.GetExpressions(tokens);
+
+        expression.Should().BeEquivalentTo(expectedExpression);
+    }
+
+    public static IEnumerable<object[]> TestCases() =>
+    new (string Source, IEnumerable<Expression> ExpectedExpression)[]
+    {
+        ("a", [Expression.VariableAccess([Token.Identifier("a", new(new(0, 0, 0), 1))])])
+    }.Select(x => new object[] { new Parser().Parse(x.Source), x.ExpectedExpression });
 }
---
+++ NewLang.Core/Expression.cs
@@ -0,0 +1,45 @@
+﻿namespace NewLang.Core;
+
+public record struct Expression(ExpressionType Type, IEnumerable<Token> Tokens)
+{
+    public static Expression VariableAccess(IEnumerable<Token> tokens)
+    {
+        return new Expression(ExpressionType.VariableAccess, tokens);
+    }
+}
+
+public enum ExpressionType
+{
+    VariableAccess
+}
+
+public class ExpressionBuilder
+{
+    public IEnumerable<Expression> GetExpressions(IEnumerable<Token> tokens)
+    {
+        using var tokensEnumerator = tokens.GetEnumerator();
+        var expression = PopExpression(tokensEnumerator);
+        while (expression.HasValue)
+        {
+            yield return expression.Value;
+            expression = PopExpression(tokensEnumerator);
+        }
+    }
+
+    private Expression? PopExpression(IEnumerator<Token> tokens)
+    {
+        while (tokens.MoveNext())
+        {
+            var token = tokens.Current;
+            switch (token.Type)
+            {
+                case TokenType.Identifier:
+                    return Expression.VariableAccess([token]);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        return null;
+    }
+}
