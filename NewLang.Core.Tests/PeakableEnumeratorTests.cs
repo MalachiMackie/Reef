1e92506e42b01300e94964bc0d78bee3a2721afd
Malachi Mackie <malachi@mackie.co.nz>
2025-03-22 09:22:38 +1300

    Peakable enumerator

---
+++ NewLang.Core.Tests/PeakableEnumeratorTests.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using FluentAssertions;
+
+namespace NewLang.Core.Tests;
+
+public class PeakableEnumeratorTests
+{
+    private readonly PeakableEnumerator<int> _sut = new(((IEnumerable<int>) [1, 2, 3, 4, 5]).GetEnumerator());
+
+    [Fact]
+    public void Should_EnumerateCorrectly_When_NotPeaked()
+    {
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(1);
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(2);
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(3);
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(4);
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(5);
+        _sut.MoveNext().Should().BeFalse();
+    }
+
+    [Fact]
+    public void Should_PeakFirstValueCorrectly()
+    {
+        _sut.TryPeak(out var peaked).Should().BeTrue();
+        peaked.Should().Be(1);
+    }
+
+    [Fact]
+    public void Should_PeakLastValueCorrectly()
+    {
+        _sut.MoveNext();
+        _sut.MoveNext();
+        _sut.MoveNext();
+        _sut.MoveNext();
+        _sut.TryPeak(out var peaked).Should().BeTrue();
+        peaked.Should().Be(5);
+    }
+
+    [Fact]
+    public void Should_ReturnFalse_When_NoValueToPeak()
+    {
+        _sut.MoveNext();
+        _sut.MoveNext();
+        _sut.MoveNext();
+        _sut.MoveNext();
+        _sut.MoveNext();
+        _sut.TryPeak(out var peaked).Should().BeFalse();
+        peaked.Should().Be(0);
+    }
+
+    [Fact]
+    public void Should_ContinueEnumeratingCorrectly_When_Peaked()
+    {
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(1);
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(2);
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(3);
+
+        _sut.TryPeak(out var peaked).Should().BeTrue();
+        peaked.Should().Be(4);
+
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(4);
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(5);
+        _sut.MoveNext().Should().BeFalse();
+    }
+
+    [Fact]
+    public void Should_PeakTwiceCorrectly()
+    {
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(1);
+        _sut.TryPeak(out var peaked1).Should().BeTrue();
+        peaked1.Should().Be(2);
+        _sut.TryPeak(out var peaked2).Should().BeTrue();
+        peaked2.Should().Be(2);
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(2);
+        _sut.MoveNext().Should().BeTrue();
+        _sut.Current.Should().Be(3);
+    }
+
+    [Fact]
+    public void Should_DisposeOfInnerEnumerator()
+    {
+        var outer = new DisposeTestEnumerator();
+        outer.Disposed.Should().BeFalse();
+        
+        var peakable = new PeakableEnumerator<int>(outer);
+        peakable.Dispose();
+
+        outer.Disposed.Should().BeTrue();
+    }
+
+    private class DisposeTestEnumerator : IEnumerator<int>
+    {
+        private readonly IEnumerator<int> _enumeratorImplementation = null!;
+        public bool MoveNext()
+        {
+            return _enumeratorImplementation.MoveNext();
+        }
+
+        public void Reset()
+        {
+            _enumeratorImplementation.Reset();
+        }
+
+        int IEnumerator<int>.Current => _enumeratorImplementation.Current;
+
+        object? IEnumerator.Current => ((IEnumerator)_enumeratorImplementation).Current;
+
+        public bool Disposed { get; private set; }
+        public void Dispose()
+        {
+            Disposed = true;
+        }
+    }
+}
---
+++ NewLang.Core/PeakableEnumerator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+
+namespace NewLang.Core;
+
+[MustDisposeResource]
+public class PeakableEnumerator<T>(IEnumerator<T> inner) : IEnumerator<T>
+{
+    private T? _peaked;
+    // cant rely on _peaked being not null, because T maybe nullable, and null may be a valid value
+    private bool _hasPeakedValue;
+    private bool _hasPeaked;
+
+    public bool TryPeak([MaybeNullWhen(false)]out T peaked)
+    {
+        if (_hasPeaked)
+        {
+            peaked = _hasPeakedValue ? _peaked : default;
+            return _hasPeakedValue;
+        }
+
+        _hasPeaked = true;
+        _hasPeakedValue = inner.MoveNext();
+        _peaked = _hasPeakedValue ? inner.Current : default;
+        peaked = _peaked;
+
+        return _hasPeakedValue;
+    }
+
+    public bool MoveNext()
+    {
+        if (_hasPeaked)
+        {
+            _hasPeaked = false;
+            return _hasPeakedValue;
+        }
+
+        return inner.MoveNext();
+    }
+
+    public void Reset()
+    {
+        _peaked = default;
+        _hasPeaked = false;
+        inner.Reset();
+    }
+
+    public T Current => _hasPeaked ? _peaked! : inner.Current;
+
+    T IEnumerator<T>.Current => Current;
+
+    object? IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+        _peaked = default;
+        _hasPeaked = false;
+        inner.Dispose();
+    }
+}
