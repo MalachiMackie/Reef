bdb8789bc7bf02802fabd15e1eeb95931c08fae1
Malachi Mackie <malachi@mackie.co.nz>
2025-07-13 19:29:52 +1200

    Update IL types

--- Reef.IL/ReefMethod.cs
+++ Reef.IL/ReefMethod.cs
@@ -5,6 +5,7 @@ public class ReefMethod
     public required IReadOnlyList<IInstruction> Instructions { get; set; }
     public required IReadOnlyList<Local> Locals { get; set; }
     public required ReefType ReturnType { get; set; }
+    public required IReadOnlyList<ReefType> TypeParameters { get; set; }
 
     public class Local
     {
--- Reef.IL/ReefType.cs
+++ Reef.IL/ReefType.cs
@@ -3,20 +3,15 @@
 public class ReefType
 {
     public required string DisplayName { get; set; }
-    
     public required IReadOnlyList<ReefVariant> Variants { get; set; }
-    
     public required IReadOnlyList<ReefMethod> Methods { get; set; }
-    
-    public required uint SizeBytes { get; set; }
-    
     public required bool IsValueType { get; set; } 
+    public required IReadOnlyList<ReefType> TypeParameters { get; set; }
 }
 
 public class ReefVariant
 {
     public required string DisplayName { get; set; }
-    
     public required IReadOnlyList<ReefField> Fields { get; set; }
 }
 
