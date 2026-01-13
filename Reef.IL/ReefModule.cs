ce97963474bce1217d1639cc217bf36bec25b5df
Malachi Mackie <malachi@mackie.co.nz>
2026-01-14 12:47:48 +1300

    remove IL and Benchmarks

--- Reef.IL/ReefModule.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Reef.IL;
-
-public class ReefModule
-{
-    public required IReadOnlyList<ReefTypeDefinition> Types { get; set; }
-    public required IReadOnlyList<ReefMethod> Methods { get; set; }
-    public required ReefMethod? MainMethod { get; set; }
-}
