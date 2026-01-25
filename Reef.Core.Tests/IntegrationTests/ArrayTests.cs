6ccd9d5a4822c33d054a935fcfaa4e67299159f9
Malachi Mackie <malachi@mackie.co.nz>
2026-01-25 15:48:55 +1300

    more array tests

--- Reef.Core.Tests/IntegrationTests/ArrayTests.cs
+++ Reef.Core.Tests/IntegrationTests/ArrayTests.cs
@@ -31,6 +31,52 @@ public class ArrayTests : IntegrationTestBase
     }
 
     [Fact]
+    public async Task FillArray()
+    {
+        await SetupTest(
+            """
+            var a = [6; 3];
+            print_i32(a[0]);
+            print_i32(a[1]);
+            print_i32(a[2]);
+            """);
+
+        var result = await Run();
+        result.ExitCode.Should().Be(0);
+        result.StandardOutput.Should().Be("012");
+    }
+
+    [Fact]
+    public async Task UnboxedArray()
+    {
+        await SetupTest("""
+                        var a = [unboxed; 1, 5];
+                        print_i32(a[0]);
+                        print_i32(a[1]);
+                        """);
+
+        var result = await Run();
+        result.ExitCode.Should().Be(0);
+        result.StandardOutput.Should().Be("15");
+    }
+
+    [Fact]
+    public async Task BoxedTypeInsideBoxedArray()
+    {
+        await SetupTest(
+            """
+            class MyClass{pub field MyString: string, }
+            var a = [new MyClass{MyString = "hi"}, new MyClass{MyString = "bye"}];
+            print_string(a[0].MyString);
+            print_string(a[1].MyString);
+            """);
+
+        var result = await Run();
+        result.ExitCode.Should().Be(0);
+        result.StandardOutput.Should().Be("hibye");
+    }
+
+    [Fact]
     public async Task WriteIntoArray()
     {
         await SetupTest(
